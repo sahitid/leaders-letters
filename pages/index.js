007ce91ba7316d4af65d4a96c5f4be6aca433f1e
Sahiti Dasari <sahitiaws@gmail.com>
2024-05-29 00:14:43 -0400

    fix highlighted article

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import HeadObject from '../components/head'
+import HeadObject from '../components/head';
 import Nav from '../components/nav';
 import { getLetters } from '../lib/LettersData';
 
@@ -10,6 +10,8 @@ export async function getStaticProps() {
 
 export default function Home({ letters }) {
   console.log(letters);
+  const [firstLetter, ...otherLetters] = letters; // Destructure the first letter separately
+
   return (
     <div className="dark:text-white dark:bg-black">
       <HeadObject>
@@ -22,38 +24,36 @@ export default function Home({ letters }) {
           <h1 className='text-5xl font-bold'>Letters From Leaders</h1>
           <h2 className='text-xl font-semibold'>... to Leaders</h2>
         </div>
-        <a href='https://hackclub.slack.com/team/U03RU99SGKA' target='blank'>
-        <button className='bg-blue-500 border-2 border-black font-semibold text-white w-32 rounded-lg px-2 py-1 text-sm'>Message @sahiti on Slack to Submit Your Own!</button>
-      </a>
+        <a href='https://hackclub.slack.com/team/U03RU99SGKA' target='_blank'>
+          <button className='bg-blue-500 border-2 border-black font-semibold text-white w-32 rounded-lg px-2 py-1 text-sm'>Message @sahiti on Slack to Submit Your Own!</button>
+        </a>
       </div>
 
-      <div className='flex flex-row items-center justify-center gap-4'>
-        <div className='relative w-96 h-64'>
-          <img className='rounded-xl border-2 border-black' src='https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg' />
-          <p className='px-2 py-1 bg-white border-2 border-black text-sm font-medium absolute top-2 left-2 rounded-md'>date</p>
-
-        </div>
-
-        <div className='flex flex-col'>
-          <h3 className='font-semibold text-lg'>title</h3>
-          <p>first 100 chars</p>
-        </div>
-
-
-      </div>
+      {firstLetter && (
+        <a href={`/letter/${firstLetter.slug}`}>
+          <div className='flex flex-row items-center justify-center gap-4 mb-10'>
+            <div className='relative w-96 h-64'>
+              <img className='rounded-xl border-2 border-black' src='https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg' alt='Letter Thumbnail' />
+              <p className='px-2 py-1 bg-white border-2 border-black text-sm font-medium absolute top-2 left-2 rounded-md'>{firstLetter.date}</p>
+            </div>
+            <div className='flex flex-col'>
+              <h3 className='font-semibold text-lg'>{firstLetter.title}</h3>
+              <p>{firstLetter.description.substring(0, 100)}...</p>
+            </div>
+          </div>
+        </a>
+      )}
 
       <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 mx-10'>
-        {letters.map((l, i) => (
-          <a href={`/letter/${l.slug}`}>
-            <div key={i} className='flex flex-col bg-purple-300 p-5 rounded-lg border-2 border-black items-center justify-center gap-2 h-48'>
+        {otherLetters.map((l, i) => (
+          <a key={i} href={`/letter/${l.slug}`}>
+            <div className='flex flex-col bg-purple-300 p-5 rounded-lg border-2 border-black items-center justify-center gap-2 h-48'>
               <p className='px-2 py-1 bg-white border-2 border-black text-sm font-medium rounded-md'>{l.date}</p>
               <h3 className='font-semibold'>{l.title}</h3>
             </div>
           </a>
         ))}
       </div>
-
-
     </div>
   )
 }
