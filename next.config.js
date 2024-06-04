9d8465fbfa768a24518c0061b530073eb3abe008
Sahiti Dasari <sahitiaws@gmail.com>
2024-06-04 15:16:43 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: '/clubs/leaders-letters',
+  async rewrites() {
+    return [
+      { source: '/clubs/leaders-letters/_next/:path*', destination: '/_next/:path*' }
+    ]
+  }
+  
 }
 
 module.exports = nextConfig
